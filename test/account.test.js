cd00c48a793985cbbefd999071356f2a7f6bce90
Christopher Ly <demedial@gmail.com>
2018-04-24 16:30:16 +0100

    Add test and source folder, pass first test

---
+++ test/account.test.js
@@ -0,0 +1,14 @@
+let expect = require("chai").expect;
+let Account = require("../src/account.js");
+
+describe("Account class", function() {
+  beforeEach(function() {
+    testAccount = new Account();
+  });
+
+  describe("Initialisation", function() {
+    it("has an empty balance of 0", function() {
+      expect(testAccount.balance).to.equal(0);
+    });
+  });
+});
